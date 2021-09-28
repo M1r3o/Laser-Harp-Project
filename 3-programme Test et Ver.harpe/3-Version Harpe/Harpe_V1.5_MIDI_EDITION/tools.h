715c91c02987cf701b35dff3fa51269d19462755
M1r3o <75146453+M1r3o@users.noreply.github.com>
2021-09-28 22:36:25 +0200

    Update tools.h

--- 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.5_MIDI_EDITION/tools.h
+++ 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.5_MIDI_EDITION/tools.h
@@ -3,10 +3,16 @@
 --MIDI Function--
 *******************
 */
-void noteOn(int cmd, int pitch, int velocity) {
+void noteOn(int cmd, int pitch) {
   Serial.write(cmd);
   Serial.write(pitch);
-  Serial.write(velocity);
+  Serial.write(127);
+}
+
+void noteOff(int cmd, int pitch) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(0);
 }
 
 void Variation(int cmd, int pitch, int velocity){
