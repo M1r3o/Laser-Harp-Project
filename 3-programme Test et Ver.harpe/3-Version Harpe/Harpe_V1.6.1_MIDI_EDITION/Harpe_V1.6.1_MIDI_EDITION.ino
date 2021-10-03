40d876c6f6dc4365bb89b22ab5a5a6ec5466705e
M1r3o <75146453+M1r3o@users.noreply.github.com>
2021-10-03 12:23:54 +0200

    Update Harpe_V1.6.1_MIDI_EDITION.ino

--- 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.6.1_MIDI_EDITION/Harpe_V1.6.1_MIDI_EDITION.ino
+++ 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.6.1_MIDI_EDITION/Harpe_V1.6.1_MIDI_EDITION.ino
@@ -1,6 +1,6 @@
 //release by M1r3o & Frederic Wauters
 //Creative Commons CC-BY-NC-SA 2021
-//Harpe_V1.5_MIDI_Edition
+//Harpe_V1.6.1_MIDI_Edition
 
 // Created by Frederic Wauters and Istchenko de Carvalho Marco under Creative Commons Licence CC-BY-NC
 // This code is delivered with no warranties and you use it at your own risks.
@@ -23,8 +23,8 @@
 //              if changes were made. You may do so in any reasonable manner, but not
 //              in any way that suggests the licensor endorses you or your use.
 //
-// Complete informations about CC-BY-NC licence :
-//    https://creativecommons.org/licenses/by-nc/2.0/be/deed.en
+// Complete informations about CC-BY-NC-SA licence :
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.en
 // Cree par Frederic Wauters et Marco Istchenko de Carvalho sous licence Creative Commons CC-BY-NC
 // Ce code est fourni sans aucune garantie et vous l'utilisez a vos risques et perils.
 // Version 1.0 - 2021-03-04 - F. Wauters Belgium
@@ -47,7 +47,7 @@
 //                      avez utilise son travail.
 //
 // Informations complètes concernant la licence CC-BY-NC :
-//    https://creativecommons.org/licenses/by-nc/2.0/be/deed.fr
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.fr
 
 
 // /!\ to send notes , we have configured serial protocol to 115200 Bytes/seconds
