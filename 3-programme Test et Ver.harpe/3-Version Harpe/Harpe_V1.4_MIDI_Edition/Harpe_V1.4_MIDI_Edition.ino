d52472852062102cd33641f846544ebdd996d259
M1r3o <75146453+M1r3o@users.noreply.github.com>
2021-10-03 12:30:26 +0200

    Update Harpe_V1.4_MIDI_Edition.ino

--- 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.4_MIDI_Edition/Harpe_V1.4_MIDI_Edition.ino
+++ 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.4_MIDI_Edition/Harpe_V1.4_MIDI_Edition.ino
@@ -1,54 +1,53 @@
 //release by M1r3o & Frederic Wauters
-//Creative Commons CC-BY-NC 2021
+//Creative Commons CC-BY-NC-SA 2021
 //Harpe_V1.4_MIDI_Edition
 
 // Created by Frederic Wauters and Istchenko de Carvalho Marco under Creative Commons Licence CC-BY-NC
 // This code is delivered with no warranties and you use it at your own risks.
-// Version 1.0 - 2021-08-12 - F. Wauters Belgium - M. Istchenko de Carvalho France 
+// Version 1.0 - 2021-03-04 - F. Wauters Belgium
 
 // PLEASE READ THIS IMPORTANT NOTICE
 // SVP LISEZ CETTE NOTE IMPORTANTE
 //
-// About CC-BY-NC...
+// About CC-BY-NC-SA...
 // -----------------
-// You CAN    : share, copy, redistribute, adapt, remix, transform and build using 
+// You CAN    : share, copy, redistribute, adapt, remix, transform and build using
 //              the code.
 //
 // You CANNOT : use for commercial purposes.
 //
-// You CANNOT : apply legal terms or technological measures that legally restrict 
+// You CANNOT : apply legal terms or technological measures that legally restrict
 //              others from doing anything the license permits.
 //
-// You MUST   : give appropriate credit, provide a link to the license, and indicate 
+// You MUST   : give appropriate credit, provide a link to the license, and indicate
 //              if changes were made. You may do so in any reasonable manner, but not
 //              in any way that suggests the licensor endorses you or your use.
 //
-// Complete informations about CC-BY-NC licence : 
-//    https://creativecommons.org/licenses/by-nc/2.0/be/deed.en
+// Complete informations about CC-BY-NC-SA licence :
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.en
 // Cree par Frederic Wauters et Marco Istchenko de Carvalho sous licence Creative Commons CC-BY-NC
 // Ce code est fourni sans aucune garantie et vous l'utilisez a vos risques et perils.
 // Version 1.0 - 2021-03-04 - F. Wauters Belgium
 //
-// A propos de CC-BY-NC
+// A propos de CC-BY-NC-SA
 // --------------------
-// Vous POUVEZ        : partager, copier, distribuer et communiquer le code par tous 
+// Vous POUVEZ        : partager, copier, distribuer et communiquer le code par tous
 //                      moyens et sous tous formats.
 //
 // Vous NE POUVEZ PAS : faire un usage commercial de ce code, tout ou partie le composant.
 //
 // Vous NE POUVEZ PAS : appliquer des conditions legales ou des mesures techniques qui
-//                      restreindraient legalement autrui a utiliser le code dans les 
+//                      restreindraient legalement autrui a utiliser le code dans les
 //                      conditions decrites par la licence.
 //
-// Vous DEVEZ         : crediter le code, integrer un lien vers la licence et indiquer si 
-//                      des modifications ont ete effectuees au code. Vous devez indiquer 
-//                      ces informations par tous les moyens raisonnables, sans toutefois 
-//                      suggerer que l'Offrant vous soutient ou soutient la facon dont vous 
+// Vous DEVEZ         : crediter le code, integrer un lien vers la licence et indiquer si
+//                      des modifications ont ete effectuees au code. Vous devez indiquer
+//                      ces informations par tous les moyens raisonnables, sans toutefois
+//                      suggerer que l'Offrant vous soutient ou soutient la facon dont vous
 //                      avez utilise son travail.
 //
-// Informations complètes concernant la licence CC-BY-NC : 
-//    https://creativecommons.org/licenses/by-nc/2.0/be/deed.fr
-
+// Informations complètes concernant la licence CC-BY-NC-SA :
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.fr
 
 // /!\ to send notes , we have configured serial protocol to 115200 Bytes/seconds
 // we used to send MIDI notes : Hairless MIDI Serial (to send MIDI notes from Arduino to Standalone Synthesizer via USB->MIDI converter)
