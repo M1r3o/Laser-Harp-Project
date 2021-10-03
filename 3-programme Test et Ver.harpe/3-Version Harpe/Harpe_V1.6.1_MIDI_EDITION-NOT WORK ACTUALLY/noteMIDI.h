a5e1f6967e273b48507512af4a3fc11c10d39be2
M1r3o <75146453+M1r3o@users.noreply.github.com>
2021-10-03 09:54:44 +0200

    Add files via upload

---
+++ 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.6.1_MIDI_EDITION-NOT WORK ACTUALLY/noteMIDI.h
@@ -0,0 +1,19 @@
+
+/*
+ ***************************
+  note midi pour laser harp
+ ***************************
+*/
+
+int note1 = 0 ;
+int note2 = 0 ;
+int note3 = 0 ;
+int note4 = 0 ;
+int note5 = 0 ;
+int note6 = 0 ;
+int note7 = 0 ;
+int note8 = 0 ;
+int note9 = 0 ;
+int note10 = 0 ;
+int note11 = 0 ;
+int note12 = 0 ;
