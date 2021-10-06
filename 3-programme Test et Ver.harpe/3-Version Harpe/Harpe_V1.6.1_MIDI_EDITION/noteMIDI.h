e399d38bfcbdc450e85d08582be43c29fa6c986c
M1r3o <75146453+M1r3o@users.noreply.github.com>
2021-10-06 11:07:11 +0200

    Add files via upload

---
+++ 3-programme Test et Ver.harpe/3-Version Harpe/Harpe_V1.6.1_MIDI_EDITION/noteMIDI.h
@@ -0,0 +1,71 @@
+//release by M1r3o & Frederic Wauters
+//Creative Commons CC-BY-NC-SA 2021
+//Harpe_V1.6.1_MIDI_Edition
+
+// Created by Frederic Wauters and Istchenko de Carvalho Marco under Creative Commons Licence CC-BY-NC-SA
+// This code is delivered with no warranties and you use it at your own risks.
+// Version 1.0 - 2021-03-04 - F. Wauters Belgium - M . istchenko de Carvalho France
+
+// PLEASE READ THIS IMPORTANT NOTICE
+// SVP LISEZ CETTE NOTE IMPORTANTE
+//
+// About CC-BY-NC-SA...
+// -----------------
+// You CAN    : share, copy, redistribute, adapt, remix, transform and build using
+//              the code.
+//
+// You CANNOT : use for commercial purposes.
+//
+// You CANNOT : apply legal terms or technological measures that legally restrict
+//              others from doing anything the license permits.
+//
+// You MUST   : give appropriate credit, provide a link to the license, and indicate
+//              if changes were made. You may do so in any reasonable manner, but not
+//              in any way that suggests the licensor endorses you or your use.
+//
+// Complete informations about CC-BY-NC-SA licence :
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.en
+// Cree par Frederic Wauters et Marco Istchenko de Carvalho sous licence Creative Commons CC-BY-NC-SA
+// Ce code est fourni sans aucune garantie et vous l'utilisez a vos risques et perils.
+// Version 1.0 - 2021-03-04 - F. Wauters Belgium - M . istchenko de Carvalho France
+//
+// A propos de CC-BY-NC-SA
+// --------------------
+// Vous POUVEZ        : partager, copier, distribuer et communiquer le code par tous
+//                      moyens et sous tous formats.
+//
+// Vous NE POUVEZ PAS : faire un usage commercial de ce code, tout ou partie le composant.
+//
+// Vous NE POUVEZ PAS : appliquer des conditions legales ou des mesures techniques qui
+//                      restreindraient legalement autrui a utiliser le code dans les
+//                      conditions decrites par la licence.
+//
+// Vous DEVEZ         : crediter le code, charegrer un lien vers la licence et indiquer si
+//                      des modifications ont ete effectuees au code. Vous devez indiquer
+//                      ces informations par tous les moyens raisonnables, sans toutefois
+//                      suggerer que l'Offrant vous soutient ou soutient la facon dont vous
+//                      avez utilise son travail.
+//
+// Informations complètes concernant la licence CC-BY-NC-SA :
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.fr
+
+
+
+/*
+ ***************************
+  note midi pour laser harp
+ ***************************
+*/
+
+char note1 = 0 ;
+char note2 = 0 ;
+char note3 = 0 ;
+char note4 = 0 ;
+char note5 = 0 ;
+char note6 = 0 ;
+char note7 = 0 ;
+char note8 = 0 ;
+char note9 = 0 ;
+char note10 = 0 ;
+char note11 = 0 ;
+char note12 = 0 ;
