c5f9d32f7b2eca3fa388001f67b77d6f0452a2a3
M1r3o <75146453+M1r3o@users.noreply.github.com>
2021-12-19 23:45:02 +0100

    Update test_Sensor_Ana_et_Dig.ino

--- 2-Sensor/test_Sensor_Ana_et_Dig/test_Sensor_Ana_et_Dig.ino
+++ 2-Sensor/test_Sensor_Ana_et_Dig/test_Sensor_Ana_et_Dig.ino
@@ -7,9 +7,10 @@
 
 int capteur = 0 ; 
 int capteur1 = 0;
+int delai = 1 ; 
 
 void setup() {
-  Serial.begin(9600);// put your setup code here, to run once:
+  Serial.begin(115200);// put your setup code here, to run once:
 
 }
 
@@ -18,8 +19,8 @@ void loop() {
   capteur1=analogRead(A0);
   Serial.println("Statue Capteur Digital (1 ou 0)");
   Serial.println(capteur);// put your main code here, to run repeatedly:
-  delay(1);
+  delay(delai);
   Serial.println("Statue Capteur analogique");
   Serial.println(capteur1);
-  delay(1);
+  delay(delai);
 }
