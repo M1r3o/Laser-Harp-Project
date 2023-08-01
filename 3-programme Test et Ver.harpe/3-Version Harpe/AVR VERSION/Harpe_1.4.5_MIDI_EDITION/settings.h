3aa5c3671148abeaa194535e9837583e356a8373
M1r3o <75146453+M1r3o@users.noreply.github.com>
2023-08-01 15:16:15 +0200

    Add files via upload

---
+++ 3-programme Test et Ver.harpe/3-Version Harpe/AVR VERSION/Harpe_1.4.5_MIDI_EDITION/settings.h
@@ -0,0 +1,168 @@
+//release by M1r3o & Frederic Wauters
+//Creative Commons CC-BY-NC-SA 2021
+//Harpe_V1.4_MIDI_Edition
+
+// Created by Frederic Wauters and Istchenko de Carvalho Marco under Creative Commons Licence CC-BY-NC-SA
+// This code is delivered with no warranties and you use it at your own risks.
+// Version 1.0 - 2021-03-04 - F. Wauters Belgium - M . istchenko de Carvalho France
+
+// PLEASE READ THIS IMPORTANT NOTICE
+// SVP LISEZ CETTE NOTE IMPORTANTE
+//
+// About CC-BY-NC-SA...
+// -----------------
+// You CAN    : share, copy, redistribute, adapt, remix, transform and build using
+//              the code.
+//
+// You CANNOT : use for commercial purposes.
+//
+// You CANNOT : apply legal terms or technological measures that legally restrict
+//              others from doing anything the license permits.
+//
+// You MUST   : give appropriate credit, provide a link to the license, and indicate
+//              if changes were made. You may do so in any reasonable manner, but not
+//              in any way that suggests the licensor endorses you or your use.
+//
+// Complete informations about CC-BY-NC-SA licence :
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.en
+// Cree par Frederic Wauters et Marco Istchenko de Carvalho sous licence Creative Commons CC-BY-NC-SA
+// Ce code est fourni sans aucune garantie et vous l'utilisez a vos risques et perils.
+// Version 1.0 - 2021-03-04 - F. Wauters Belgium - M . istchenko de Carvalho France
+//
+// A propos de CC-BY-NC-SA
+// --------------------
+// Vous POUVEZ        : partager, copier, distribuer et communiquer le code par tous
+//                      moyens et sous tous formats.
+//
+// Vous NE POUVEZ PAS : faire un usage commercial de ce code, tout ou partie le composant.
+//
+// Vous NE POUVEZ PAS : appliquer des conditions legales ou des mesures techniques qui
+//                      restreindraient legalement autrui a utiliser le code dans les
+//                      conditions decrites par la licence.
+//
+// Vous DEVEZ         : crediter le code, integrer un lien vers la licence et indiquer si
+//                      des modifications ont ete effectuees au code. Vous devez indiquer
+//                      ces informations par tous les moyens raisonnables, sans toutefois
+//                      suggerer que l'Offrant vous soutient ou soutient la facon dont vous
+//                      avez utilise son travail.
+//
+// Informations complètes concernant la licence CC-BY-NC-SA :
+//    https://creativecommons.org/licenses/by-nc-sa/2.0/be/deed.fr
+////////////////////////////////////////////////
+//////////////////--OPTION--///////////////////
+//Pour activer ou désactiver une option, saisisez 1 pour activer ou 0 pour desactiver
+boolean NOTE_MIDI_REVERSE = 0 ;
+boolean VIBRATION_CORDE = 1 ;
+const int nombre_patch = 4 ;
+/////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////--TABLEAU DES NOTES ET VIBRATION--///////////////////////////////////////
+int patch_notes [nombre_patch][13] {
+  {9,60,65,67,68,70,71,72,74,75},
+  {9,60,65,67,68,70,71,72,74,75},
+  {9,60,65,67,68,70,71,72,74,75},
+  {9,60,65,67,68,70,71,72,74,75}, // ajouter une ligne pour ajouter un patch 
+  // {nombre_de_corde, note1, note2, etc...}, Mise en forme pour ajouter un patch 
+};
+/////////////////////////////////////////////////////////////////////////////////////
+int cpt = 0 ; // varie n fonction de l'appuie sur les pedale
+int nbCordes = patch_notes[cpt][0] + 1; // Nombre de cordes (definit automatiquement)
+//const int nbNote = nbCordes - 1 ; 
+///////////////////////////////////////////////////////////////////////////////////
+
+////////////////////--NE PAS TOUCHER LES VARIABLES ET FONCTION QUI SUIVENT--/////////////////
+int Notes[][4] = {
+  {0, patch_notes [cpt][nbCordes-1], patch_notes [cpt][1], 0},
+  {0, patch_notes [cpt][nbCordes-2], patch_notes [cpt][2], 0},
+  {0, patch_notes [cpt][nbCordes-3], patch_notes [cpt][3], 0},
+  {0, patch_notes [cpt][nbCordes-4], patch_notes [cpt][4], 0},
+  {0, patch_notes [cpt][nbCordes-5], patch_notes [cpt][5], 0},
+  {0, patch_notes [cpt][nbCordes-6], patch_notes [cpt][6], 0},
+  {0, patch_notes [cpt][nbCordes-7], patch_notes [cpt][7], 0},
+  {0, patch_notes [cpt][nbCordes-8], patch_notes [cpt][8], 0},
+  {0, patch_notes [cpt][nbCordes-9], patch_notes [cpt][9], 0},
+  {0, patch_notes [cpt][nbCordes-10], patch_notes [cpt][10], 0},
+  {0, patch_notes [cpt][nbCordes-11], patch_notes [cpt][11], 0},
+  {0, patch_notes [cpt][nbCordes-12], patch_notes [cpt][12], 0},
+};
+const int FLAGON = 0;
+int NOTE_MIDI = 0 ;
+//
+const int VIBRATION = 3 ;
+int cordeVibration = 0;
+//
+/*********************************
+  variable de travail de la harpe
+ ********************************/
+int minX = 100;
+int maxX = 3700;
+int laserPin = 2;         // Controle Laser
+int boutonOpen = 9;      // Bouton ouverture
+int boutonClose = 9;     // Bouton fermeture
+bool harpeStatut = false; // Statut (ouverte/fermee)
+bool laserStatut = false; // Statut laser (on/off)
+int pinSensor = 10 ;
+const char pedale_plus = 7 ; // pedale patch + 
+const char pedale_moins = 8 ; // pedale patch  - 
+
+int milieu = maxX - minX ;
+///////////////////////////////////////////////////////
+////////////--Variables de travail--//////////////////
+int ecart = 0;
+float position_corde = 0 ;
+/////////////////////////////////////////////////////
+// Pour les pauses : a modifier en fonction de tes propres galvo
+int pauseStabilisation = 2;
+int pausePersistence = 1;
+boolean etat = 0 ;
+
+
+////////////////--MIDI Function--////////////////
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+}
+////////////////////////////////////////////////
+/////////////////--OPTION--////////////////////
+// 1-REVERSE MODE DES NOTES
+void reverse() {
+  if (NOTE_MIDI_REVERSE == 0) {
+    NOTE_MIDI = 2 ;
+  }
+  else {
+    NOTE_MIDI = 1 ;
+  }
+}
+//2 VIBRATION CORDE
+void vibration() {
+  if (VIBRATION_CORDE == 0) {
+    cordeVibration = 0 ;
+  }
+  else {
+    cordeVibration = 100 ;
+  }
+}
+// 3 pedales
+
+void pedales() { 
+  if (digitalRead(pedale_plus) == 1) {
+    cpt++ ;
+    dac.setVoltage(milieu , false);
+    delay(500);
+  }
+  else if (digitalRead(pedale_moins) == 1) {
+    cpt-- ;
+    dac.setVoltage(milieu ,false);
+    delay(500);
+  }
+  else if (digitalRead(pedale_plus) == 1 && cpt == nombre_patch-1) {
+    cpt = 0 ;
+    dac.setVoltage(milieu , false);
+    delay(500);
+  }
+  else if (digitalRead(pedale_moins) == 1 && cpt== 0) {
+    dac.setVoltage(milieu, false);
+    delay(500);
+  }
+}
+//////////////////////////////////////////////
---
+++ 3-programme Test et Ver.harpe/Programme Annexe/OuvertureHarpeV3_Capteur_Alpha/OuvertureHarpeV3_Capteur_Alpha.ino
@@ -0,0 +1,207 @@
+// Petit cours Arduino pour Marco...
+// ---------------------------------
+// En dehors de tous les coide, on peut faire les déclarations qui servent a toutes les fonctions
+// et importer les eventuelles librairies necessaires.
+//
+// Les lignes de commentaires commencent par "//" et ne sont pas prises en compte
+// par le programme ni par le compilateur. Ils ne servent qu'aux developpeurs.
+//
+// Tous programmes Arduino doit au moins avoir un void setup et un void loop.
+// Attention : il faut toujours respecter la casse !
+//-------------------------------------------------------------------------------------------------------
+
+// Librairie pour le DAC
+#include <MCP4725.h>
+#include <MIDIUSB.h>
+// Declarations
+MCP4725 dac(SDA, SCL);    // DAC
+int laserPin = 4;         // COntrole Laser
+int boutonOpen = 3;      // Bouton ouverture
+int boutonClose = 4;     // Bouton fermeture
+int nbCordes = 14;         // Nombre de cordes
+bool harpeStatut = false; // Statut (ouverte/fermee)
+bool laserStatut = false; // Statut laser (on/off)
+int Pin_Sensor_Note = 3 ;
+unsigned long Delay_Motor = 2;
+int LDR_Sensor_Note = 450;//ce n'est qu'une valeur que j'ai choisie 
+
+// Pour les pauses : a modifier en fonction de tes propres servos
+int pauseStabilisation = 1;
+int pausePersistence = 1;
+
+// Pour le debattement de la harpe
+int minX = 1129;
+int maxX = 3237;
+
+// Variables de travail
+int ecart = 0;
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+// *********
+// * SETUP *
+// *********
+void setup() {
+  // Le Setup sert principalement a configurer et
+  // a initialiser le diverses choses qui regissent le projet.
+  // Si on y fait tourner autre chose, c'est pour "preparer le terrain", rien d'autre
+  Serial.begin(115200);
+  // Initialisation des pins
+  pinMode(laserPin, OUTPUT);
+  pinMode(boutonOpen, INPUT);
+  pinMode(boutonClose, INPUT);
+
+  // S'assurer que le laser est eteint
+  digitalWrite(laserPin, LOW);
+
+  // Initialisation du DAC
+  dac.begin();
+
+  dac.setValue(1129);
+  delay(500);
+  dac.setValue(2047);
+  delay(500);
+  dac.setValue(3237);
+  delay(500);
+  dac.setValue(2047);
+  delay(1000);
+}
+
+// ****************
+// * Lecture Note *
+// ****************
+
+void loop() {
+  // Le loop est la partie qui est "rejouee" en permanence par l'Arduino.
+  // C'est la raison de son nom. Lorsque tou y a ete execute, il recommence.
+
+  dac.setValue(2047);
+
+  // Attendre qu'on appuie sur open
+  while (digitalRead(boutonOpen) == LOW) {
+    // On ne fait rien, on attend qu'on appuie
+    // Cette boucle tourne tant que le bouton open n'est pas enfonce
+  }
+
+  // On a appuie, on allume le laser
+  digitalWrite(laserPin, HIGH);
+  laserStatut = true;                // on retient que le laser est allume
+
+  // On attend un peu pour l'effet... enfin, si on veut
+  delay(1000);
+
+  // SEQUENCE D'OUVERTURE
+  // On boucle pour un ecart de 0 jusque maximum. L'ecart = 4095/(nbCordes-1)
+  for (int ecart = 0; ecart < ((maxX - minX) / (nbCordes - 1)); ecart++) {
+    // PLACER LE GALVO
+    // On boucle sur toutes les cordes pour placer chacune
+    for (int cordeCourante = 0; cordeCourante < (nbCordes - 1); cordeCourante++) {
+      // Eteindre le laser
+      digitalWrite(laserPin, LOW);
+      laserStatut = false;
+
+      // Placer le galvo
+      dac.setValue((2047 - (ecart * nbCordes / 2)) + (cordeCourante * ecart));
+
+      // Pause de stabilisation
+     delay(pauseStabilisation);
+
+    }
+
+    // Allumer le laser
+    digitalWrite(laserPin, HIGH);
+    laserStatut = true;
+
+    // Pause persistence
+    delay(pausePersistence);
+
+    // La harpe est ouverte, on le retient
+    harpeStatut =  true;
+
+    // Attendre qu'on appuie sur open
+    while (digitalRead(boutonOpen) == LOW) {
+      // Ecart
+      ecart = (maxX - minX) / (nbCordes - 1);
+
+      // En attendant qu'on demande la fermeture, on dessine la harpe et ses faisceaux bien sur...
+      // On boucle sur toutes les cordes pour placer chacune
+      for (int cordeCourante = 0; cordeCourante < (nbCordes - 1); cordeCourante++) {
+        // Eteindre le laser
+        digitalWrite(laserPin, LOW);
+        laserStatut = false;
+
+        // Placer le galvo
+        dac.setValue((2047 - (ecart * nbCordes / 2)) + (cordeCourante * ecart));
+
+        // Pause de stabilisation
+         while (millis() - temps0 < Delay_Motor) {
+        //digitalWrite(PinLaser , HIGH);
+        if (analogRead(Pin_Sensor_Note) > LDR_Sensor_Note ) {
+          noteOn(0, 48 + cordeCourante, 64);
+          MidiUSB.flush();
+          delay(250) ;
+         
+
+
+        } else {
+          noteOff(0, 48 + cordeCourante, 64); // Channel 0, middle C, normal velocity
+          MidiUSB.flush();
+
+        }
+
+      }
+
+
+        // Allumer le laser
+        digitalWrite(laserPin, HIGH);
+        laserStatut = true;
+        //mettre Note MIDI
+        // Pause persistence
+        delay(pausePersistence);
+      }
+    }
+
+    // Si on a quitte la boucle, c'est qu'on a demande la fermeture
+
+    // SEQUENCE DE FERMETURE
+    // On boucle pour un ecart maximum jusque 0. L'ecart = 4095/(nbCordes-1)
+    for (int ecart = (maxX - minX) / (nbCordes - 1); ecart > 0; ecart--) {
+      // PLACER LE GALVO
+      // On boucle sur toutes les cordes pour placer chacune
+      for (int cordeCourante = 0; cordeCourante < (nbCordes - 1); cordeCourante++) {
+        // Eteindre le laser
+        digitalWrite(laserPin, LOW);
+        laserStatut = false;
+
+        // Placer le galvo
+        dac.setValue((2047 - (ecart * nbCordes / 2)) + (cordeCourante * ecart));
+
+        // Pause de stabilisation
+        delay(pauseStabilisation);
+
+        // Allumer le laser
+        digitalWrite(laserPin, HIGH);
+        laserStatut = true;
+
+        // Pause persistence
+        delay(pausePersistence);
+      }
+    }
+
+
+  }
+  // Harpe fermee (et on eteint le laser)
+  harpeStatut = false;
+  digitalWrite(laserPin, LOW);
+  laserStatut = false;                // on retient que le laser est allume
+
+  // On est au bout du LOOP, l'Arduino va recommencer. Il attend donc qu'on appuie a nouveau sur Ouvrir...
+}
